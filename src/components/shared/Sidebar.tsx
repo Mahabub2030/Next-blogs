440e4ea72a0c5a5ed20dd8a235320e6dcc380ba0
MAHABUB-ALAM <mahabubalom72091@gmail.com>
2025-09-28 17:24:10 +0300

    added login code

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { Home, PlusCircle, LogOut } from "lucide-react";
+import { Home, LogOut, PlusCircle } from "lucide-react";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 
 export default function Sidebar() {
+  const session = useSession();
+  console.log(session);
   return (
     <aside className="flex h-screen w-64 flex-col border-r bg-black text-white">
       {/* Top navigation */}
@@ -28,16 +31,16 @@ export default function Sidebar() {
 
       {/* Bottom action */}
       <div className="p-4 border-t border-gray-500">
-        <Button
-          variant="destructive"
-          className="w-full justify-start gap-2 cursor-pointer"
-          onClick={() => {
-            console.log("Logout clicked");
-          }}
-        >
-          <LogOut className="h-4 w-4" />
-          Logout
-        </Button>
+        {session?.status === "authenticated" && (
+          <Button
+            variant="destructive"
+            className="w-full justify-start gap-2 cursor-pointer"
+            onClick={() => signOut()}
+          >
+            <LogOut className="h-4 w-4" />
+            Logout
+          </Button>
+        )}
       </div>
     </aside>
   );
